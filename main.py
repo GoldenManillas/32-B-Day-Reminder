e20b8bf13a237f23db7c0dbfd119c53e6f15c629
GoldenManillas <16807605+GoldenManillas@users.noreply.github.com>
2026-04-06 21:56:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,84 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-import smtplib as SMTP
-import pandas as p
-import os
-from io import StringIO
+import requests as r
 import datetime as dt
+import smtplib as SMTP
 
-sender_email = os.environ.get("SENDER_EMAIL")
-reciever_email = os.environ.get("RECEIVER_EMAIL")
-sender_password = os.environ.get("PASSWORD")
-dates = p.read_csv(StringIO(os.environ.get("DATES")))
+DAYS_FOR_NOTIFICATION = 20
+message_header = "Subject:B-Day Reminder\n\nHey, just a heads up,\n"
 
-message_header = "Subject:B-Day Reminder\n\nHey, just a heads up,"
+sender_email     = os.environ.get("SENDER_EMAIL")
+reciever_email   = os.environ.get("RECEIVER_EMAIL")
+sender_password  = os.environ.get("PASSWORD")
+date_url         = os.environ.get("DATE_URL")
 
-date_dict = {}
-for (index, data_row) in dates.iterrows():
-    new_key = (data_row.month, data_row.day)
-    if new_key in date_dict:
-        current_names = date_dict[new_key]
-        current_names.append(data_row["name"])
-        date_dict[new_key] = current_names
+print("Starting")
+resp = r.get(date_url)
+
+resp.raise_for_status()
+row_json = resp.json()["sheet1"]
+
+procesed_list = []
+events_list = []
+
+for x in row_json:
+
+    now = dt.datetime.now()
+    target = now.date().replace(day = x["day"], month=x["month"])
+    if (now.date() > target):
+        target = target.replace(year=target.year + 1)
+
+
+    time_delta = target - now.date()
+    if "nonBirthdayMessage" in x:
+
+        if(time_delta.days < DAYS_FOR_NOTIFICATION):
+            new_dict = {"name":x["name"], "daysUntilB-Day":time_delta.days, "message":x["nonBirthdayMessage"]}
+            events_list.append(new_dict)
     else:
-        date_dict[new_key] = [data_row["name"]]
 
-tuples_for_the_next_week = []
-now = dt.datetime.now()
-for x in range(0, 8):
-    end_date = now + dt.timedelta(days=x)
-    tuples_for_the_next_week.append((end_date.month, end_date.day))
-
-message_body = ""
-counter = 0
-for date_tuple in tuples_for_the_next_week:
-    if date_tuple in date_dict:
-        name_array = date_dict[date_tuple]
-        name_string = ""
-        match(len(name_array)):
-            case 0:
-                pass
-            case 1:
-                name_string += name_array[0] + "'s"
-            case 2:
-                name_string += name_array[0] + " and " + name_array[1] + "'s"
-            case _:
-                last_index = len(name_array) - 1
-                
-                for name in name_array[0:last_index]:
-                    name_string += name + ", "
-                name_string += "and " + name_array[last_index] + "'s"
+        if(time_delta.days < DAYS_FOR_NOTIFICATION):
+            new_dict = {"name":x["name"], "daysUntilB-Day":time_delta.days}
+            procesed_list.append(new_dict)
 
 
-        match counter:
-            case 0:
-                message_body += f"It's {name_string} Birthday today, you should get on that." + "\n"
-            case _:
-                message_body += f"{name_string} Birthday is in {counter} days, FYI." + "\n"
-    counter+=1
+ordered_list = {}
+for entry in procesed_list:
+    if entry["daysUntilB-Day"] in ordered_list:
+        ordered_list[entry["daysUntilB-Day"]].append(entry["name"])
+    else:
+        ordered_list[entry["daysUntilB-Day"]] = [entry["name"]]
 
 
-if message_body != "":
+final_message_string = ""
+for num_days in sorted(ordered_list):
+    name_list = ordered_list[num_days]
+    if len(name_list) > 1:
+        result = ", ".join(name_list[:-1]) + ", and " + name_list[-1]
+        posessive = "have"
+    else:
+        result = "".join(name_list)
+        posessive = "has"
+
+    if num_days > 1:
+        message_string = f"{result} {posessive} a birthday in {num_days} days. Make sure you're on that!"
+    elif num_days == 1:
+        message_string = f"{result} {posessive} a birthday tomorrow. Make sure you're on that!"
+    else:
+        message_string = f"{result} {posessive} a birthday today. Make sure you send a text!"
+
+    final_message_string += message_string + "\n"
+
+final_message_string += "\n"
+
+for event in events_list:
+    final_message_string += f"Heads up, {event["name"]} is in {num_days} days. {event["message"]}"
+
+
+if final_message_string != "":
     connection = SMTP.SMTP("smtp.gmail.com", port=587)
     connection.starttls()
     connection.login(user=sender_email, password=sender_password)
-    email_body = f"{message_header}{message_body}"
+    email_body = f"{message_header}{final_message_string}"
     connection.sendmail(from_addr=sender_email, to_addrs=reciever_email, msg=email_body)
     connection.close()
+
+    
